a3c3d41b2006a2e40d9fe9aaf99990fdf5b15628
fawyd <fabian.wydler@gmail.com>
2012-07-27 10:43:08 +0200

    About page commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SampleApp::Application.routes.draw do
+  get "static_pages/about"
+  
   get "static_pages/home"
 
   get "static_pages/help"
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,4 +9,20 @@ describe "Static pages" do
       page.should have_content('Sample App')
     end
   end
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_content('Help')
+    end
+  end
+
+  describe "About page" do
+    
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_content('About Us')
+    end
+  end
 end
